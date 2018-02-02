6f669aedde122c70eb8da0d12e87caee25763ac1
AlexanderPampushko <pampushko@gmail.com>
2018-02-02 22:24:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Описание проекта
+ 
+Телеграм-бот
+ 
+## Мотивация
+ 
+Никакой мотивации
+ 
+## Contributors
+ 
+Alexander Pampushko
+ 
+## License
+ 
+BSD license
---
+++ src/main/java/pampushko/telegram/Bot1Application.java
@@ -0,0 +1,14 @@
+package pampushko.telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bot1Application
+{
+	
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Bot1Application.class, args);
+	}
+}
